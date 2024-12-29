e1c800708288c21de9611ed569d7f29550eff7ec
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-29 19:16:47 +0600

    feat(header):init_-_home--marge

--- src/Pages/Home/Header/Header.jsx
+++ src/Pages/Home/Header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Category from './Category/Category';
 
 const Header = () => {
     return (
         <div>
+            <div className='text-center'>
+                <h1 className='italic text-xl  text-yellow-400 '>--From 11:00am to 10:00pm--</h1>
 
+                <div>
+                    <div className='divider divider-warning w-[25%] mx-auto'></div>
+                    <div className='italic lg:text-4xl md:text-3xl text-lg'>
+                        Order Online
+                    </div>
+                    <div className='divider divider-warning w-[25%] mx-auto'></div>
+                </div>
+            </div>
+
+
+
+            <Category />
         </div>
     );
 };
