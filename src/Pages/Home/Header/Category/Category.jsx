82a11b41063e0ec2580744ad425c89d6b050c19f
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-29 23:18:07 +0600

    fix(category):compelete_category--added

--- src/Pages/Home/Header/Category/Category.jsx
+++ src/Pages/Home/Header/Category/Category.jsx
@@ -15,16 +15,16 @@ const Category = () => {
         { id: 1, name: 'Salad', image: 'https://i.ibb.co.com/2yL4Lkb/slide1.jpg' },
         { id: 2, name: 'Soup', image: 'https://i.ibb.co.com/64MLj5X/slide3.jpg' },
         { id: 3, name: 'Desert', image: 'https://i.ibb.co.com/brWFZrN/slide4.jpg' },
-        { id: 4, name: 'Pizza', image: 'https://images.pexels.com/photos/2619970/pexels-photo-2619970.jpeg?auto=compress&cs=tinysrgb&w=300' },
-        { id: 5, name: 'Fashion', image: 'https://via.placeholder.com/150' },
-        { id: 6, name: 'Education', image: 'https://via.placeholder.com/150' },
-        { id: 7, name: 'Fitness', image: 'https://via.placeholder.com/150' },
-        { id: 8, name: 'Lifestyle', image: 'https://via.placeholder.com/150' },
+        { id: 4, name: 'Pizza', image: 'https://images.pexels.com/photos/2619970/pexels-photo-2619970.jpeg?auto=compress&cs=tinysrgb&w=900' },
+        { id: 5, name: 'Coffee', image: 'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?auto=compress&cs=tinysrgb&w=900' },
+        { id: 6, name: 'Cold Drinks', image: 'https://images.pexels.com/photos/2789328/pexels-photo-2789328.jpeg?auto=compress&cs=tinysrgb&w=1200' },
+        { id: 7, name: 'Burger', image: 'https://images.pexels.com/photos/1600727/pexels-photo-1600727.jpeg?auto=compress&cs=tinysrgb&w=1000' },
+        { id: 8, name: 'Fruits', image: 'https://images.pexels.com/photos/1132047/pexels-photo-1132047.jpeg?auto=compress&cs=tinysrgb&w=1200' },
     ];
 
     return (
         <div>
-            <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Categories</h2>
+            <h2 className='text-yellow-400 font-semibold italic md:text-xl lg:text-2xl text-sm' style={{ textAlign: 'center', marginBottom: '20px' }}>Categories</h2>
             <Swiper
                 slidesPerView={4} // Default number of slides
                 spaceBetween={30}
