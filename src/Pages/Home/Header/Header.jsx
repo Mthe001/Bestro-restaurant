218a000848ba89f483940156d09839d58e5aed80
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-29 23:51:10 +0600

    feat(sectionTitle):initalize-dynamic--__--title-component

--- src/Pages/Home/Header/Header.jsx
+++ src/Pages/Home/Header/Header.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import Category from './Category/Category';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const Header = () => {
     return (
         <div>
-            <div className='text-center'>
-                <h1 className='italic md:text-xl text-sm  lg:text-2xl   text-yellow-400 '>--From 11:00am to 10:00pm--</h1>
+            <section>
+                <SectionTitle
+                    subHeading={'-- From 11:00am to 10:00pm --'}
+                    heading={"Order Online"}>
 
-                <div>
-                    <div className='divider divider-warning w-[25%] mx-auto'></div>
-                    <div className='italic font-sm dark:text-orange-600 text-black lg:text-4xl md:text-3xl text-lg'>
-                        Order Online
-                    </div>
-                    <div className='divider divider-warning w-[25%] mx-auto'></div>
-                </div>
-            </div>
+                </SectionTitle>
+            </section>
 
 
 
---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SectionTitle = ({ heading, subHeading }) => {
+    return (
+        <div className='text-center'>
+            <p className='italic md:text-xl text-sm  lg:text-2xl  text-yellow-400 '>{subHeading}</p>
+            <div className='divider divider-warning w-[25%] mx-auto'></div>
+            <h3 className='text-xl md:text-xl lg:text-2xl text-yellow-500 dark:text-orange-500'>{heading}</h3>
+            <div className='divider divider-warning w-[25%] mx-auto'></div>
+        </div>
+    );
+};
+
+export default SectionTitle;
