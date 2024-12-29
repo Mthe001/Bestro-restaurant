d93a96cd6ce5b6c30818234fa9b516386f0f0b02
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-30 05:15:05 +0600

    feat(bst-boss):init-home

--- src/Pages/Home/BistroBossLatter/BistroBoss.jsx
+++ src/Pages/Home/BistroBossLatter/BistroBoss.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import chefImg from '../../../assets/home/chef-services.png';
 
 const BistroBoss = () => {
     return (
-        <div>
-            <h1>Bistro Boss</h1>
+        <div className='py-10'>
+            <div className='w-full relative'>
+                {/* Image */}
+                <img className='w-[80%] mx-auto' src={chefImg} alt="chef-services" />
+
+                {/* White background div */}
+                <div className='absolute z-50 top-5 md:top-10 lg:top-16 left-0 right-0 mx-auto w-[50%] rounded-lg h-[60%] bg-white flex items-center justify-center'>
+                    <h1 className="text-center">Bistro Boss</h1>
+                    <p></p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Header from './Header/Header';
+import BistroBoss from './BistroBossLatter/BistroBoss';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <Header />
+            <BistroBoss />
         </div>
     );
 };
