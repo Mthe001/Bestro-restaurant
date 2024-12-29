74cd78106b19bb7b9aa0e1a7012012940a125b6d
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-29 19:46:10 +0600

    feat(category):category__header

--- src/Pages/Home/Header/Category/Category.jsx
+++ src/Pages/Home/Header/Category/Category.jsx
@@ -6,17 +6,16 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import './Category.css'; // You can add custom styles here if needed
+import './Category.css';
 
-// Import required modules
 import { Pagination } from 'swiper/modules';
 
 const Category = () => {
     const categories = [
-        { id: 1, name: 'Technology', image: 'https://via.placeholder.com/150' },
-        { id: 2, name: 'Health', image: 'https://via.placeholder.com/150' },
-        { id: 3, name: 'Travel', image: 'https://via.placeholder.com/150' },
-        { id: 4, name: 'Food', image: 'https://via.placeholder.com/150' },
+        { id: 1, name: 'Salad', image: 'https://i.ibb.co.com/2yL4Lkb/slide1.jpg' },
+        { id: 2, name: 'Soup', image: 'https://i.ibb.co.com/64MLj5X/slide3.jpg' },
+        { id: 3, name: 'Desert', image: 'https://i.ibb.co.com/brWFZrN/slide4.jpg' },
+        { id: 4, name: 'Pizza', image: 'https://images.pexels.com/photos/2619970/pexels-photo-2619970.jpeg?auto=compress&cs=tinysrgb&w=300' },
         { id: 5, name: 'Fashion', image: 'https://via.placeholder.com/150' },
         { id: 6, name: 'Education', image: 'https://via.placeholder.com/150' },
         { id: 7, name: 'Fitness', image: 'https://via.placeholder.com/150' },
@@ -62,9 +61,9 @@ const Category = () => {
                                 src={category.image}
                                 alt={category.name}
                                 style={{
-                                    width: '120px',
-                                    height: '120px',
-                                    borderRadius: '50%',
+                                    width: '260px',
+                                    height: '280px',
+                                    borderRadius: '3px',
                                     objectFit: 'cover',
                                     margin: '0 auto',
                                 }}
