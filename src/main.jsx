582d310a3f4c9d235b9b9f0a4d05af162beaf9c5
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-31 04:44:48 +0600

    feat(main):react-helmet-async-initialize

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './routes/Routes';
-import { ThemeProvider } from './Provider/ThemeContext'; // Import the ThemeProvider
+import { ThemeProvider } from './Provider/ThemeContext';
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider> {/* Use ThemeProvider instead of ThemeContext.Provider */}
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <HelmetProvider>
+      <ThemeProvider> {/* Use ThemeProvider instead of ThemeContext.Provider */}
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </HelmetProvider>
   </StrictMode>
 );
