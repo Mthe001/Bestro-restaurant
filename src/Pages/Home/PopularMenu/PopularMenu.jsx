fe8c3c5162148d218c1502a4c1f3809c16b4d789
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-31 04:58:16 +0600

    feat(home):emplement-check-it-details--using(navigate)

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,19 +1,27 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import MenuItem from '../../../shared/MenuItem/MenuItem';
 
 const PopularMenu = () => {
     const [menu, setMenu] = React.useState([]);
+    const navigate = useNavigate(); // Hook for navigation
+
     useEffect(() => {
         fetch('menu.json')
             .then(res => res.json())
             .then(data => {
                 const popularItems = data.filter(item => item.category === 'popular');
-                setMenu(popularItems)
-            })
-    }, [])
+                setMenu(popularItems);
+            });
+    }, []);
+
+    const handleOrderNow = () => {
+        navigate('/menu'); // Navigate to the "Order" page
+    };
+
     return (
-        <section>
+        <section className='my-6'>
             <SectionTitle
                 heading='Form Our Menu'
                 subHeading='-- Popular Items --'
@@ -27,6 +35,16 @@ const PopularMenu = () => {
                         item={item}
                     ></MenuItem>)
                 }
+            </div>
+
+            {/* "Order Now" Button */}
+            <div className="flex justify-center ">
+                <button
+                    onClick={handleOrderNow}
+                    className="btn btn-outline border-yellow-400 hover:bg-yellow-300  hover:border-transparent  border-b-4 border-0 transition-all"
+                >
+                    Check in Details
+                </button>
             </div>
         </section>
     );
