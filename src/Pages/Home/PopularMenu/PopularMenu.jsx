5febcef02aa40997603f68c0613ff178813e63c9
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-31 02:08:06 +0600

    fix(pupular-menu):added my-10 for better ui looks

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -20,7 +20,7 @@ const PopularMenu = () => {
             >
             </SectionTitle>
 
-            <div className='grid grid-cols-1 lg:grid-cols-2  gap-5 md:grid-cols-2'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 my-10 gap-5 md:grid-cols-2'>
                 {
                     menu.map(item => <MenuItem
                         key={item._id}
