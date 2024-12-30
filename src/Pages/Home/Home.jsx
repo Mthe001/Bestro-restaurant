efd2db2f33a8dd711c89542e90b15af6a0e40fad
MThe001 <mdredwanulhaque01614591672@gmail.com>
2024-12-31 04:48:12 +0600

    feat(menu--home):react-helmet-emplement

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,10 +5,14 @@ import BistroBoss from './BistroBossLatter/BistroBoss';
 import PopularMenu from './PopularMenu/PopularMenu';
 import Featured from './Featured/Featured';
 import Testimonials from './Testimonials/Testimonials';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Home | Bestro Boss</title>
+            </Helmet>
             <Banner />
             <Header />
             <BistroBoss />
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import { Helmet } from 'react-helmet-async';
 const Menu = () => {
     return (
         <div>
+            <Helmet>
+                <title>Menu | Bestro Boss</title>
+            </Helmet>
             <h1>Menu</h1>
         </div>
     );
