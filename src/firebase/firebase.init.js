2e18c5b0ef428221f0173c64af014f761342204b
MThe001 <mdredwanulhaque01614591672@gmail.com>
2025-01-01 08:11:47 +0600

    fix:firebase-secire-init

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,26 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBLU5KynpnZFc2sCnvvjEuOHb3nKisx44I",
-//   authDomain: "bestro-boss-3447a.firebaseapp.com",
-//   projectId: "bestro-boss-3447a",
-//   storageBucket: "bestro-boss-3447a.firebasestorage.app",
-//   messagingSenderId: "1026603055128",
-//   appId: "1:1026603055128:web:d33c39ce89bcde7ffcab6d"
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
-
-
-// // Initialize Firebase Authentication and get a reference to the service
-// const auth = getAuth(app);
-
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
