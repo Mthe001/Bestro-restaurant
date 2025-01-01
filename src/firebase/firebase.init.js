e7c8dd953d9e5924e1fa7fd7d860dfac15693290
MThe001 <mdredwanulhaque01614591672@gmail.com>
2025-01-01 08:10:51 +0600

    feat:firebase-secure

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,21 +1,40 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBLU5KynpnZFc2sCnvvjEuOHb3nKisx44I",
+//   authDomain: "bestro-boss-3447a.firebaseapp.com",
+//   projectId: "bestro-boss-3447a",
+//   storageBucket: "bestro-boss-3447a.firebasestorage.app",
+//   messagingSenderId: "1026603055128",
+//   appId: "1:1026603055128:web:d33c39ce89bcde7ffcab6d"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+// // Initialize Firebase Authentication and get a reference to the service
+// const auth = getAuth(app);
+
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBLU5KynpnZFc2sCnvvjEuOHb3nKisx44I",
-  authDomain: "bestro-boss-3447a.firebaseapp.com",
-  projectId: "bestro-boss-3447a",
-  storageBucket: "bestro-boss-3447a.firebasestorage.app",
-  messagingSenderId: "1026603055128",
-  appId: "1:1026603055128:web:d33c39ce89bcde7ffcab6d"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
-
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
+// Initialize Firebase Authentication
+export const auth = getAuth(app);
